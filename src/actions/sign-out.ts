4a4235062099a027e499861902c624f77d734f30
Abunawas <abunawas.arifandi@gmail.com>
2024-07-08 21:38:25 +0700

    course 84

--- src/actions/sign-out.ts
+++ src/actions/sign-out.ts
@@ -1,7 +1,9 @@
 'use server'
 
 import * as auth from '@/auth'
+import { signOut as nextAuthSignOut } from 'next-auth/react'
 
 export async function signOut() {
-    return auth.signOut()
+    await auth.signOut()
+    await nextAuthSignOut({ redirect: false })
 }
